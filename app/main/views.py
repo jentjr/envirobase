e2e530e92ec09fb85a2ceaf1e62f9d0516920840
Justin Jent <jentjr@gmail.com>
2018-12-12 22:49:21 -0500

    start working on templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,18 @@
 from . import main
+from datetime import datetime
 from .forms import SiteForm
 from .. import db
 from ..models import Site, Unit
 from flask import render_template, redirect, url_for, abort, flash, request, current_app, make_response
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form  = SiteForm()
-    return render_template('index.html', form=form)
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@main.route('/site/<name>', methods=['GET', 'POST'])
+def site():
+    return render_template('site.html', name=name)
 
 
 
