d9797508b1f23b1589af7434ebda02dcc44e30ab
Justin Jent <jentjr@gmail.com>
2018-12-16 22:34:08 -0500

    return geojson

--- envirobase.py
+++ envirobase.py
@@ -1,5 +1,6 @@
+import json
 import psycopg2
-import psycopg2.extras
+from psycopg2.extras import DictCursor
 from config import config
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, request
@@ -26,21 +27,32 @@ def index():
 @app.route("/site")
 def site():
     conn = connect_db()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory = DictCursor)
     try:
-        cur.execute("SELECT site_name, address, city, state, zipcode FROM SITE")
-    except:
-        print("Error executing select")
+        cur.execute(
+            """
+            SELECT row_to_json(fc)
+              FROM ( SELECT 'FeatureCollection' AS type, array_to_json(array_agg(f)) AS features
+              FROM (SELECT 'Feature' AS type
+                 , ST_AsGeoJSON(sg.site_geog)::json AS geometry
+                 , row_to_json((SELECT s FROM (SELECT site_id, site_name, address, city, state, zipcode) AS s
+                    )) AS properties
+               FROM site AS sg   ) AS f )  AS fc
+            """
+        )
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     sites = cur.fetchall()
+    sites = sites[0][0]
     return render_template("site.html", sites=sites)
 
 @app.route("/parameter")
 def parameter():
     conn = connect_db()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory = DictCursor)
     try:
         cur.execute("SELECT param_cd, group_name, srsname, description, parameter_unit FROM sample_parameter")
-    except:
-        print("Error executing select")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     parameters = cur.fetchall()
     return render_template("parameter.html", parameters=parameters)
